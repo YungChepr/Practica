32ffd50d514d8b3299279fd2016af4b95e2085ed
YungChepr <chandy.ru@gmail.com>
2023-07-07 20:34:15 +0700

    Сделал оранджевый круг на темном фоне

--- src/Main.java
+++ src/Main.java
@@ -31,7 +31,16 @@ class MyDrawPanel extends JPanel {
     public void paintComponent(Graphics g) {
         //g.setColor(Color.orange);
         //g.fillRect(20, 50, 100, 100);
-        Image image = new ImageIcon("holo.jpg").getImage();
-        g.drawImage(image,3,4,this);
+        //Image image = new ImageIcon("holo.jpg").getImage();
+        //g.drawImage(image,3,4,this);
+        g.fillRect(0,0,this.getWidth(), this.getHeight());
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+        Color randomColor = new Color(red, green, blue);
+        g.setColor(randomColor);
+        g.fillOval(70,70,100,100);
+
+
     }
 }
